f2c1957a894ca3c78df333a85d401b4a1597a54a
TheMissing32 <102628505+TheMissing32@users.noreply.github.com>
2024-04-17 22:19:36 -0400

    Addtoinventory finally actually gets item id
    Now I can actually send the data to the next page to make them appear in the inventory and lighthouse

--- public/js/addToInventory.js
+++ public/js/addToInventory.js
@@ -2,27 +2,29 @@
 //On item click, add to inventory. When clicked emit to room that object is valid (False to True).
 'use strict';
 
-AFRAME.registerComponent('addToInventory', {
+AFRAME.registerComponent('addtoinventory', {
     schema: {
         duration: {type: 'number', default:10000.0},
+        item_id: {default:'default'}
     },
 
     init: function()
     {
+        console.log("js exists");
         //const Context_AF = this;
         let socket = io('http://localhost:8080');
 
         socket.on('connect', (userData) => {
             console.log('I exist!');
         });
-
+        var data = this.data;
         let scene = document.querySelector('a-scene');
         const saveKey = document.getElementById('key');
         scene.addEventListener('click', function() {
    
-            let item_id = this.getAttribute('id');
-    
-            switch(item_id){
+            //let item_id = this.el.getAttribute('id');
+            console.log("Item ID:", data.item_id);
+            switch(data.item_id){
                 case "key":
                     console.log("Clicked item is the key");
                     break;
